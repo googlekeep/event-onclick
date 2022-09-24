4e96c03c00df6c40f758debdf409a2f0dcf8d98b
googlekeep <info.workingforyou@gmail.com>
2022-09-24 20:55:38 +0530

    button onclick

---
+++ script.js
@@ -0,0 +1,22 @@
+// obsion 2
+function makeYello(){
+    document.body.style.backgroundColor='yellow'
+}
+
+
+
+// obtion 1
+const makeBlueButton =document.getElementById('makeBlue');
+        makeBlueButton.onclick =makeAuntilBlue;
+        function makeAuntilBlue(){
+            document.body.style.backgroundColor ='blue'
+        }
+
+        // obsion3
+
+
+        const makeRedButton =document.getElementById('makered');
+        makeRedButton.onclick = function makeRed(){
+            document.body.style.backgroundColor ='red'
+        }
+       
