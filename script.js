f313e66da37952427acb8bfe8efa187988b1305b
googlekeep <info.workingforyou@gmail.com>
2022-09-24 21:40:55 +0530

    different ways to use add event listener on a button

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ const makeBlueButton =document.getElementById('makeBlue');
             document.body.style.backgroundColor ='red'
         }
        
+
+        // obsion 4
+        const pinkButton = document.getElementById('makepink');
+        pinkButton.addEventListener('click',makePink)
+    
+        function makePink(){
+            document.body.style.backgroundColor = 'pink'
+        }
+
+
+
+        // obsion 5 ( atai besi amora used korbo)
+        const makeGreenButton =document.getElementById('makegreen');
+    makeGreenButton.addEventListener('click', function makeGreen (){
+        document.body.style.backgroundColor ='green';
+    })
+
+        // obsion 6 very importuned amora r ro besi used korbo
+
+    document.getElementById('make-goldenrod').addEventListener('click',function(){
+        document.body.style.backgroundColor = 'goldenrod';
+       })
